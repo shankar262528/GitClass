82e71144a3a17a01b35b6050ad3afd4c700e438a
shankar262528 <shankar262528@gmail.com>
2023-04-16 15:04:26 +0530

    sprint

---
+++ src/test/java/com/emp/EmployeeDetails.java
@@ -0,0 +1,13 @@
+package com.emp;
+
+public class EmployeeDetails {
+	private void employee1() {
+		System.out.println("raji");
+
+	}
+	private void employee2() {
+		System.out.println("shankar");
+
+	}
+
+}
